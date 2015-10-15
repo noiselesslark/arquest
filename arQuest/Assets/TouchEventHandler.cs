d001e30729af643c1f2bcedcd1e238b49a65bdb9
noiselesslark <elisekarlsson@gmail.com>
2015-10-15 17:38:00 +0200

    working version of touchhandler

---
+++ arQuest/Assets/TouchEventHandler.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TouchEventHandler : MonoBehaviour {
+
+    void Start()
+    {
+
+    }
+
+    void OnTouchDown()
+    {
+
+    }
+
+    void OnTouchUp()
+    {
+
+    }
+
+    void OnTouchStay()
+    {
+
+    }
+
+    void OnTouchExit()
+    {
+
+    }
+}
